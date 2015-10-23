f405041c54083d3d66bbdc61f91e0cedd89fb8d0
Andrean Franc <andrean.franc@gmail.com>
2015-10-23 13:56:29 +0200

    Register opener label

--- librarian_opener_html/hooks.py
+++ librarian_opener_html/hooks.py
@@ -1,5 +1,10 @@
+from bottle_utils.i18n import lazy_gettext as _
+
 from .opener import html_opener
 
 
 def initialize(supervisor):
-    supervisor.exts.openers.register('html', html_opener, content_type='html')
+    supervisor.exts.openers.register('html',
+                                     label=_("Pages"),
+                                     route=html_opener,
+                                     content_type='html')
