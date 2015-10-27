370bee35dd77e190f53acda82fa72bd8148e2c04
Andrean Franc <andrean.franc@gmail.com>
2015-10-27 17:14:30 +0100

    Use quoted url form

--- librarian_opener_html/opener.py
+++ librarian_opener_html/opener.py
@@ -1,6 +1,6 @@
 import os
 
-from bottle import request
+from bottle_utils.html import quoted_url
 
 from librarian_content.decorators import with_meta
 from librarian_core.contrib.templates.renderer import view
@@ -10,10 +10,10 @@ from librarian_core.contrib.templates.renderer import view
 @with_meta(abort_if_not_found=False)
 def html_opener(path, meta):
     if not meta:
-        return dict(html_url=request.app.get_url('files:direct', path=path),
-                    keep_formatting=False)
+        html_url = quoted_url('files:direct', path=path)
+        return dict(html_url=html_url, keep_formatting=False)
 
     file_path = os.path.join(meta.path, meta['html']['main'])
-    html_url = request.app.get_url('files:direct', path=file_path)
+    html_url = quoted_url('files:direct', path=file_path)
     keep_formatting = meta['html']['keep_formatting']
     return dict(html_url=html_url, keep_formatting=keep_formatting)
